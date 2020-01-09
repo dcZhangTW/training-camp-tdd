07107861948ba8d683451ab7e189a0b51a652a73
dcZhangTW <dczhang@thoughtworks.com>
2020-01-09 14:17:10 +0800

    [Test-3] code green for Test-3

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -6,6 +6,8 @@ class FizzBuzz {
             return "Fizz";
         } else if (number == 5) {
             return "Buzz";
+        } else if (number == 7) {
+            return "Whizz";
         }
         return "";
     }
