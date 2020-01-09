33f5eae9b4d4ee8b9873bf2678ef4c0c5237d4aa
dcZhangTW <dczhang@thoughtworks.com>
2020-01-09 15:02:23 +0800

    [Test-5] green code

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -4,7 +4,7 @@ class FizzBuzz {
     static String report(int number) {
         if (number % 3 == 0) {
             return "Fizz";
-        } else if (number == 5) {
+        } else if (number % 5 == 0) {
             return "Buzz";
         } else if (number == 7) {
             return "Whizz";
